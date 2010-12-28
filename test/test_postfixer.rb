bc204331e43994e24d5d55bafcc9c7ad18cc348e
Bryce Kerley <bkerley@brycekerley.net>
2010-12-28 15:41:03 -0500

    postfixer test

---
+++ test/test_postfixer.rb
@@ -0,0 +1,28 @@
+require 'helper'
+
+class TestPostfixer < Test::Unit::TestCase
+  include Crapshoot::Tokens
+  def self.should_postfixify(from, to)
+    should "postfixify #{from.inspect} to #{to.inspect}" do
+      candidate = @postfixer.postfixify from
+      assert_equal to, candidate
+    end
+  end
+  context 'a postfixer' do
+    setup do
+      @postfixer = Crapshoot::Postfixer.new
+    end
+
+    five = Constant.new '5'
+    plus = Arithmetic.new '+'
+    minus = Arithmetic.new '-'
+    six = Constant.new '6'
+    series1 = Series.new '5', '6', nil
+    series2 = Series.new '4', '20', 'v'
+
+    should_postfixify([five, plus, six],
+                      [five, six, plus])
+    should_postfixify([series1, plus, six, minus, series2],
+                      [series1, six, plus, series2, minus])
+  end
+end
