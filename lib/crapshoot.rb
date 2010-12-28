7c5773fd1af93b33175619ff8ff67fb61bb7b7a3
Bryce Kerley <bkerley@brycekerley.net>
2010-12-28 18:06:36 -0500

    roll implementation

--- lib/crapshoot.rb
+++ lib/crapshoot.rb
@@ -3,3 +3,17 @@ require 'active_support'
 %w{ scanner postfixer evaluator }.each do |f|
   require File.join(File.dirname(__FILE__), 'crapshoot', f)
 end
+
+module Crapshoot
+  def self.roll(expression)
+    scanner = Scanner.new
+    postfixer = Postfixer.new
+    evaluator = Evaluator.new
+
+    tokens = scanner.parse expression
+    postfix_tokens = postfixer.postfixify tokens
+    result = evaluator.evaluate postfix_tokens
+
+    return result
+  end
+end
